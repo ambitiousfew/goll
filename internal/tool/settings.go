b51b13dc4f3218320b7e239ba8610598ebc43220
Coby Leuschke <43158+cglusky@users.noreply.github.com>
2025-02-27 13:48:31 -0600

    better name for package

---
+++ internal/tool/settings.go
@@ -0,0 +1,10 @@
+// package tool contains the Settings struct that is used to store the global settings for goll.
+package tool
+
+// Settings struct contains the global settings for goll.
+// Set from the settings.json file.
+type Settings struct {
+	APIBase    string `json:"api_base_url"`
+	FolderBase string `json:"folder_base_path"`
+	Timeout    int    `json:"timeout"`
+}
